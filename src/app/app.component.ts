34a4d61680a298070e0e125e6e744f68a4d959df
MohammadAssidi <mhmdassidi2002@gmail.com>
2026-03-21 16:47:47 +0200

    feat: add header component and search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
