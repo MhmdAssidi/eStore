95ec24c81e9c104f5558bae7467c5fec922ae5c7
MohammadAssidi <mhmdassidi2002@gmail.com>
2026-03-23 17:19:39 +0200

    Fetch categories from the server in side and main navigation bars.

--- src/app/components/home/sidenavigation/sidenavigation.component.ts
+++ src/app/components/home/sidenavigation/sidenavigation.component.ts
@@ -12,10 +12,26 @@ export class SidenavigationComponent {
   categories: Category[] = [];
 
   constructor(categoryService: CategoryService) {
-    this.categories = categoryService.getAllCategories();
+    categoryService.getAllCategories().subscribe(categories => {
+      this.categories = categories;
+    });
   }
 
-  getCategories(parentCategoryId?: number): Category[] {
-    return this.categories.filter(category => category.parent_category_id === parentCategoryId);
+ getCategories(parentCategoryId?: number): Category[] {
+
+  // Case 1: No id so we need main categories
+  if (parentCategoryId === undefined) {
+    return this.categories.filter(category => {
+      return category.parent_category_id === null;
+    });
+  }
+
+  // Case 2: With id so we need subcategories
+  else {
+    return this.categories.filter(category => {
+      return category.parent_category_id === parentCategoryId;
+    });
+  }
+
 }
 }
