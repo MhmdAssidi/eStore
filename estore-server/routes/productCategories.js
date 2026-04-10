76ccb1d79a42bcffb4e923f2fc396b5b90bb9a01
MohammadAssidi <mhmdassidi2002@gmail.com>
2026-04-10 21:47:22 +0300

    Use promise await in productCategories and Products backend files and fix the sql query on signup backend.

--- estore-server/routes/productCategories.js
+++ estore-server/routes/productCategories.js
@@ -1,21 +1,15 @@
-const express=require('express');
-const productCategories= express.Router();
-const pool=require('../shared/pool');
+const express = require('express');
+const productCategories = express.Router();
+const pool = require('../shared/pool');
 
-productCategories.get('/',(req,res)=>{
-   
-    pool.query('SELECT * FROM categories',(err,categories)=>{
-       if(err){
-           console.error('Error executing query:', err);
-          res.status(500).send(err);
-          return;
-            }
-        else{
-                res.status(200).send(categories);
-         }
-        });
+productCategories.get('/', async (req, res) => {
+  try {
+    const [categories] = await pool.query('SELECT * FROM categories');
+    res.status(200).send(categories);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).send(err);
+  }
+});
 
-    });  
-
-    module.exports=productCategories;
-
+module.exports = productCategories;
--- estore-server/routes/products.js
+++ estore-server/routes/products.js
@@ -1,53 +1,51 @@
-const express=require('express');
-const products=express.Router();
-const pool=require('../shared/pool');
+const express = require('express');
+const products = express.Router();
+const pool = require('../shared/pool');
 
-products.get('/', (req, res) => {
-  const mainCategoryId = req.query.maincategoryid;
-  const subCategoryId = req.query.subcategoryid;
-  const keywords= req.query.keywords;
-  let query = 'SELECT products.* FROM products';
-  const queryParams = [];
+products.get('/', async (req, res) => {
+  try {
+    const mainCategoryId = req.query.maincategoryid;
+    const subCategoryId = req.query.subcategoryid;
+    const keywords = req.query.keywords;
 
-  if (mainCategoryId) {
-  query += ' JOIN categories ON products.category_id = categories.id';
-  query += ' WHERE categories.parent_category_id = ?';
-  queryParams.push(mainCategoryId);
+    let query = 'SELECT products.* FROM products';
+    const queryParams = [];
 
-  if (keywords) {
-    query += ' AND keywords LIKE ?';
-    queryParams.push(`%${keywords}%`); 
-  }
-}
-  
-  else if (subCategoryId) {
-    query += ' WHERE products.category_id = ?';
-    queryParams.push(subCategoryId);
-  }
+    if (mainCategoryId) {
+      query += ' JOIN categories ON products.category_id = categories.id';
+      query += ' WHERE categories.parent_category_id = ?';
+      queryParams.push(mainCategoryId);
 
-  pool.query(query, queryParams, (err, products) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).send(err);
-      return;
+      if (keywords) {
+        query += ' AND keywords LIKE ?';
+        queryParams.push(`%${keywords}%`);
+      }
+    } else if (subCategoryId) {
+      query += ' WHERE products.category_id = ?';
+      queryParams.push(subCategoryId);
     }
 
-    res.status(200).send(products);
-  });
+    const [productsData] = await pool.query(query, queryParams);
+    res.status(200).send(productsData);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).send(err);
+  }
 });
 
-    products.get('/:id',(req,res)=>{
-        const productId=req.params.id;
-        pool.query('SELECT * FROM products WHERE id=?',[productId],(err,product)=>{
-           if(err){
-               console.error('Error executing query:', err);
-               res.status(500).send('Error fetching product');
-           }
-           else{
-               res.status(200).send(product);
-           }
-        });
-    });
+products.get('/:id', async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const [product] = await pool.query(
+      'SELECT * FROM products WHERE id = ?',
+      [productId]
+    );
 
-    module.exports=products;
+    res.status(200).send(product);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).send('Error fetching product');
+  }
+});
 
+module.exports = products;
