4604dc1fa6e49f775703f28657620fb9f30fbea5
MohammadAssidi <mhmdassidi2002@gmail.com>
2026-04-12 01:06:02 +0300

    Replace sign in/register button to username after log in and make the logout functionality. Plus save the local storage content even after reload.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component, Output, EventEmitter, output, signal } from '@angular/core';
-import { CategoriesStoreItem } from '../home/services/category/category.storeItem';
-import { SearchKeyword } from '../home/types/searchKeyword.type';
+import { Component, effect, output, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { CategoriesStoreItem } from '../home/services/category/category.storeItem';
+import { SearchKeyword } from '../home/types/searchKeyword.type';
 import { cartStoreItem } from '../home/services/cart/cart.storeItem';
+import { UserService } from '../home/user/services/user.service';
+
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
@@ -12,13 +16,43 @@ import { cartStoreItem } from '../home/services/cart/cart.storeItem';
 })
 export class HeaderComponent {
 
-   readonly searchClicked = output<SearchKeyword>(); 
-  displaySearch=signal(true);
-  constructor(public categoryStore: CategoriesStoreItem,private router:Router,public cart:cartStoreItem) {
-    router.events. //listens to any navigation (page change) in Angular.
-    pipe(filter(event => event instanceof NavigationEnd //We only care when the navigation is completed, not during loading. and we care for the veent of naviagtion end only thats why we filter
-    )).subscribe(() => {
-      this.displaySearch.set(router.url === '/home/products');
+  readonly searchClicked = output<SearchKeyword>();
+
+  displaySearch = signal(true);
+  isUserAuthenticated = signal(false);
+  userName = signal('');
+
+  constructor(
+    public categoryStore: CategoriesStoreItem,
+    private router: Router,
+    public cart: cartStoreItem,
+    private userService: UserService
+  ) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.displaySearch.set(this.router.url === '/home/products');
+      });
+
+    const authSignal = toSignal(this.userService.isUserAuthenticated$, {
+      initialValue: false
+    });
+
+    const loggedInUserSignal = toSignal(this.userService.loggedInUsers$, {
+      initialValue: {
+        firstname: '',
+        lastname: '',
+        address: '',
+        city: '',
+        state: '',
+        pin: '',
+        email: ''
+      }
+    });
+
+    effect(() => {
+      this.isUserAuthenticated.set(authSignal());
+      this.userName.set(loggedInUserSignal().firstname);
     });
   }
 
@@ -27,5 +61,12 @@ export class HeaderComponent {
       categoryId: Number(categoryId),
       keywords: keywords
     });
+  
+  
+  }
+  logout(event?: MouseEvent): void {
+    event?.preventDefault();
+    event?.stopPropagation();
+    this.userService.logout();
   }
 }
